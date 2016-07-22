8acaa90c7a2760a1623661b7f0ef8c8f75c01d63
kolialike <kolialike@gmail.com>
2016-07-22 11:56:49 +0300

    maks

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+jQuery(function($){
+  
+    var appendNumber = 4;
+    var prependNumber = 1;
+    var swiper = new Swiper('.swiper-container', {
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 4,
+        centeredSlides: true,
+        centeredSlides: 0,
+        paginationClickable: true,
+        spaceBetween: 20,
+        loop: true,
+    });
+    
+    
+
+	var bannerTextR = $(".banner-text-r div");
+    var bannerTextL = $(".banner-text-l div");
+	bannerTextR.on('click', function(event) {
+	    // event.preventDefault();
+	    $(this).addClass('active').siblings().removeClass('active');
+	    var index = bannerTextR.index($(this));
+	    bannerTextL.eq(index).addClass('active').siblings().removeClass('active');    
+	});
+
+
+    var headerBottomSearch = $(".header-bottom-search");
+    var body = $("body");
+    headerBottomSearch.on('click', function(event) {
+        event.preventDefault();
+        body.addClass('search-open')
+    });
+});
